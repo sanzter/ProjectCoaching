fb87b9be42a682945a2466349f128bba3ca5cdde
sanzter <peterkingjo38@gmail.com>
2025-12-16 19:09:09 +0700

    Java Project

---
+++ project.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class project {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static String[] nama = new String[3];
+    static int[] jumlah = new int[3];
+    static int[] total = new int[3];
+    static int harga = 10000;
+
+    public static void main(String[] args) throws IOException {
+        int pilihan;
+
+        do {
+            System.out.println("\n--- Peter King's Cake ---");
+            System.out.println("1. Tambah Data");
+            System.out.println("2. Tampilkan Data");
+            System.out.println("3. Ubah Data");
+            System.out.println("4. Hapus Data");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = Integer.parseInt(br.readLine());
+
+            switch (pilihan) {
+                case 1 -> tambahData();
+                case 2 -> tampilData();
+                case 3 -> ubahData();
+                case 4 -> hapusData();
+                case 5 -> System.out.println("Program selesai");
+                default -> System.out.println("Menu tidak valid");
+            }
+        } while (pilihan != 5);
+    }
+
+    // CREATE
+    static void tambahData() throws IOException {
+        for (int i = 0; i < nama.length; i++) {
+            if (nama[i] == null) {
+                System.out.print("Nama Pemesan: ");
+                nama[i] = br.readLine();
+                System.out.print("Jumlah Pesanan: ");
+                jumlah[i] = Integer.parseInt(br.readLine());
+                total[i] = jumlah[i] * harga;
+                System.out.println("Data berhasil ditambahkan");
+                return;
+            }
+        }
+        System.out.println("Data penuh!");
+    }
+
+    // Membaca Data
+    static void tampilData() {
+        System.out.println("\n--- Data Pesanan ---");
+        for (int i = 0; i < nama.length; i++) {
+            if (nama[i] != null) {
+                System.out.println((i + 1) + ". " + nama[i].toUpperCase()
+                        + "Jumlah: " + jumlah[i]
+                        + "Total: " + total[i]);
+            }
+        }
+    }
+
+    // UPDATE
+    static void ubahData() throws IOException {
+        tampilData();
+        System.out.print("Pilih nomor data yang diubah: ");
+        int index = Integer.parseInt(br.readLine()) - 1;
+
+        if (index >= 0 && index < nama.length && nama[index] != null) {
+            System.out.print("Nama baru: ");
+            nama[index] = br.readLine();
+            System.out.print("Jumlah baru: ");
+            jumlah[index] = Integer.parseInt(br.readLine());
+            total[index] = jumlah[index] * harga;
+            System.out.println("Data berhasil diubah");
+        } else {
+            System.out.println("Data tidak valid");
+        }
+    }
+
+    // DELETE
+    static void hapusData() throws IOException {
+        tampilData();
+        System.out.print("Pilih nomor data yang dihapus: ");
+        int index = Integer.parseInt(br.readLine()) - 1;
+
+        if (index >= 0 && index < nama.length && nama[index] != null) {
+            nama[index] = null;
+            jumlah[index] = 0;
+            total[index] = 0;
+            System.out.println("Data berhasil dihapus");
+        } else {
+            System.out.println("Data tidak valid");
+        }
+    }
+}
